a0151ad41e6095e0f27d170799c3ef6844765325
Dmitrii Eremin <dmitrii.eremin@withsecure.com>
2023-06-24 22:17:19 +0300

    new

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import quote
 
 html: str = '<html><head>' + \
 '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">' +\
@@ -9,8 +10,11 @@ for root, dirs, files in os.walk(".", topdown=False):
    for name in files:
       if name.lower().endswith('.html') and root.count('/') <= 1:
         href: str = os.path.join(root, name)
-        html += f'<li><a href="{href}">{root} --- {name}</a></li>'
+        encoded = f'https://github.com/alkmena5/recipes/blob/main/{quote(href[2:])}'
+        html += f'<li><a href="https://htmlpreview.github.io/?{encoded}">{root} --- {name}</a></li>'
 html += f'</ul></body></html>'
 
 with open('index.html', 'w') as f:
    f.write(html)
+
+
